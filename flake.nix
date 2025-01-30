b8bfae7de306b51568ec1d4a2e89a87bcc5e4473
killuayz <2959243019@qq.com>
2025-01-30 17:10:49 +0800

    修改nix配置文件，增加了pyhton和nodejs的支持

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "flake for easydb";
 
   inputs = {
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs.url = "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }: {
@@ -20,6 +21,15 @@
             flex 
             nlohmann_json
             readline
+            nodejs_20
+            nodePackages.npm
+            (
+              pkgs.python3.withPackages (
+                python-pkgs: [
+                  python-pkgs.websockets
+                ]
+              )
+            )
           ];
 
           shellHook = ''
@@ -34,7 +44,25 @@
         pkgs = import nixpkgs { system = "x86_64-linux"; config = {}; overlays = []; };
       in  
       {
-        default = pkgs.callPackage ./easydb.nix {};
+        default = pkgs.stdenv.mkDerivation {
+          name = "easydb";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ cmake pkg-config clang ];
+          buildInputs = with pkgs; [ flex bison nlohmann_json readline ];
+
+          cmakeFlags = [
+            "-DCMAKE_C_COMPILER=${pkgs.clang}/bin/clang"
+            "-DCMAKE_CXX_COMPILER=${pkgs.clang}/bin/clang++"
+          ];
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/
+            cp -r bin $out/
+            cp -r test $out/
+            runHook postInstall
+          '';
+        };
       };
   };
 }
